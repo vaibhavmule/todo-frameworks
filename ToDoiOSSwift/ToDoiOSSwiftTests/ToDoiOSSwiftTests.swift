1c954d894198dd1b2cc72c8f46bbf4d89f37dd28
prakash sejwani <prakashsejwani@gmail.com>
2016-03-11 17:10:10 +0530

    Add framework ios-swift

---
+++ ToDoiOSSwift/ToDoiOSSwiftTests/ToDoiOSSwiftTests.swift
@@ -0,0 +1,36 @@
+//
+//  ToDoiOSSwiftTests.swift
+//  ToDoiOSSwiftTests
+//
+//  Created by prakash sejwani on 3/11/16.
+//  Copyright © 2016 prakash sejwani. All rights reserved.
+//
+
+import XCTest
+@testable import ToDoiOSSwift
+
+class ToDoiOSSwiftTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}

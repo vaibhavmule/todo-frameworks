31758fee96d8c3ab06083a3c5121851a78a518be
prakash sejwani <prakashsejwani@gmail.com>
2016-03-18 13:52:48 +0530

    Add name and completed column in todo migration

--- todo-ruby-rails/db/migrate/20160314125321_create_todos.rb
+++ todo-ruby-rails/db/migrate/20160314125321_create_todos.rb
@@ -1,7 +1,8 @@
 class CreateTodos < ActiveRecord::Migration
   def change
     create_table :todos do |t|
-
+      t.string :name
+      t.boolean :completed, default: false
       t.timestamps null: false
     end
   end

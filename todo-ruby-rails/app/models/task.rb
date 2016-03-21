f884be3aa001e8ede33c53300144de7529b99476
prakash sejwani <prakashsejwani@gmail.com>
2016-03-21 16:13:51 +0530

    Add one to many realtionship between project and task

--- todo-ruby-rails/app/models/task.rb
+++ todo-ruby-rails/app/models/task.rb
@@ -1,4 +1,6 @@
 class Task < ActiveRecord::Base
 
   validates :name, presence: true
+
+  belongs_to: :project
 end
--- todo-ruby-rails/db/schema.rb
+++ todo-ruby-rails/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160321093538) do
+ActiveRecord::Schema.define(version: 20160321095207) do
 
   create_table "projects", force: :cascade do |t|
     t.string   "name"
@@ -24,7 +24,10 @@ ActiveRecord::Schema.define(version: 20160321093538) do
     t.boolean  "completed",  default: false
     t.datetime "created_at",                 null: false
     t.datetime "updated_at",                 null: false
+    t.integer  "project_id"
   end
+
+  add_index "tasks", ["project_id"], name: "index_tasks_on_project_id"
 
   create_table "todos", force: :cascade do |t|
     t.string   "name"

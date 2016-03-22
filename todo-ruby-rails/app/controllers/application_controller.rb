2e37c2445141e68486f80378751486d5e22df7a7
prakash sejwani <prakashsejwani@gmail.com>
2016-03-22 16:07:46 +0530

    Add root false default serializer option

--- todo-ruby-rails/app/controllers/application_controller.rb
+++ todo-ruby-rails/app/controllers/application_controller.rb
@@ -2,4 +2,11 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_action :default_serializer_options
+
+  private
+  def default_serializer_options
+    {root: false}
+  end
 end

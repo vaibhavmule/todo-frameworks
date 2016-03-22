a6ac1219097721e4f87152fe4571818000d1d02b
prakash sejwani <prakashsejwani@gmail.com>
2016-03-22 16:08:45 +0530

    Add slug column in project and add friendly_url method in project model

--- todo-ruby-rails/app/models/project.rb
+++ todo-ruby-rails/app/models/project.rb
@@ -1,4 +1,16 @@
 class Project < ActiveRecord::Base
 
   has_many :tasks, dependent: :destroy
+
+  extend FriendlyId
+  friendly_id :name, use: [:slugged, :finders]
+
+  before_save :update_slug
+
+  private
+  def update_slug
+    if slug.blank?
+      self.slug = (name).to_url
+    end
+  end
 end

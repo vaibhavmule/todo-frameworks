b4bdf7548b045669060a6dd17bf314b8d77d550f
Prakash Sejwani <prakash.sejwani@kalpavruksh.com>
2017-11-15 12:23:38 +0530

    Fix jslint for enberKeyPress function

--- reactjs/src/components/TodoApp.js
+++ reactjs/src/components/TodoApp.js
@@ -1,48 +1,45 @@
-import React, { Component } from 'react';
-import AppBar from 'material-ui/AppBar';
+import React, { Component } from "react";
+import AppBar from "material-ui/AppBar";
 
-import AddTodo from './AddTodo.js'
-import TodoList from './TodoList.js'
-import {rightMargin} from './Styles.js'
-
+import AddTodo from "./AddTodo.js";
+import TodoList from "./TodoList.js";
+import { rightMargin } from "./Styles.js";
 
 export default class TodoApp extends Component {
-	state = {
-		todoList: [],
-		todoItem: ''
-	}
+  state = {
+    todoList: [],
+    todoItem: ""
+  };
 
-	handleChange = e => {
-		this.setState({[e.target.name]: e.target.value})
-	}
+  handleChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
 
-	onEnterKeyPress = e => {
-		e.key === 'Enter' ? this.addTodoToList(this.state.todoItem) : null
-	}
+  onEnterKeyPress = e => {
+    return e.key === "Enter" && this.addTodoToList(this.state.todoItem);
+  };
 
-	addTodoToList = todo => {
-		const todoList = this.state.todoList
-		todoList.push(todo)
-		this.setState({todoList, todoItem: ''})
-	}
+  addTodoToList = todo => {
+    const todoList = this.state.todoList;
+    todoList.push(todo);
+    this.setState({ todoList, todoItem: "" });
+  };
 
-	render() {
-		const {todoList, todoItem} = this.state
-		return (
-			<div>
-			  <AppBar
-			    title="Todo App"
-			  />
-			  <div style={rightMargin}>
-					<AddTodo 
-						todo={todoItem}
-						handleChange={this.handleChange}
-						add={this.addTodoToList}
-						onEnterKeyPress={this.onEnterKeyPress}
-						/>
-					<TodoList todos={todoList}/>
-				</div>
+  render() {
+    const { todoList, todoItem } = this.state;
+    return (
+      <div>
+        <AppBar title="Todo App" />
+        <div style={rightMargin}>
+          <AddTodo
+            todo={todoItem}
+            handleChange={this.handleChange}
+            add={this.addTodoToList}
+            onEnterKeyPress={this.onEnterKeyPress}
+          />
+          <TodoList todos={todoList} />
+        </div>
       </div>
-		)
-	}
+    );
+  }
 }

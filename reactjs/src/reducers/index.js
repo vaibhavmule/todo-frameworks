fe82da737bccba654932794a605236abf65ae7d0
Vaibhav Mule <vaibhavmule135@gmail.com>
2017-11-16 17:11:15 +0530

    add initialState and modifyTodo reducer

--- reactjs/src/reducers/index.js
+++ reactjs/src/reducers/index.js
@@ -1,15 +1,23 @@
 import { combineReducers } from 'redux'
-import {ADD_TODO} from '../actions'
+import { ADD_TODO, MODIFY_TODO } from '../actions'
 
-const todos = (state=[], action) => {
+const initialState = {
+  todo: '',
+  todos: []
+}
+
+const todos = (state=initialState, action) => {
   switch (action.type) {
     case ADD_TODO:
-      return [
-        ...state,
-        {
-          todo: action.todo,
-        }
-      ]
+      return Object.assign({},
+        state,
+        { todos: [...state.todos, action.todo],
+          todo: ''
+        })
+    case MODIFY_TODO:
+      return Object.assign({},
+        state,
+        { todo: action.todo })
     default:
       return state
   }

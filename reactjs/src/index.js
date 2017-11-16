b3b4f1f80af0188d4b2a4bacf0572697850751dc
Vaibhav Mule <vaibhavmule135@gmail.com>
2017-11-16 14:43:57 +0530

    add store

--- reactjs/src/index.js
+++ reactjs/src/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+
 import './index.css';
 import App from './App';
+import rootReducer from './reducers'
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+let store = createStore(rootReducer) // this is store
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+	document.getElementById('root')
+);
 registerServiceWorker();

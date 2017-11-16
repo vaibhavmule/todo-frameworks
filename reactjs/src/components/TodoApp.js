215a82ba364c3007f3f5595a107fc5e10a246a3f
Vaibhav Mule <vaibhavmule135@gmail.com>
2017-11-16 17:14:12 +0530

    remove methods which moved to containers

--- reactjs/src/components/TodoApp.js
+++ reactjs/src/components/TodoApp.js
@@ -1,41 +1,25 @@
 import React, { Component } from "react";
+import { connect } from 'react-redux'
 import AppBar from "material-ui/AppBar";
 
-import AddTodo from "./AddTodo.js";
+import AddTodo from "../containers/AddTodo.js";
 import TodoList from "../containers/TodoList.js";
 import { rightMargin } from "./Styles.js";
 
-export default class TodoApp extends Component {
-
-  handleChange = e => {
-    this.setState({ [e.target.name]: e.target.value });
-  };
-
-  onEnterKeyPress = e => {
-    return e.key === "Enter" && this.addTodoToList(this.state.todoItem);
-  };
-
-  addTodoToList = todo => {
-    const todoList = this.state.todoList;
-    todoList.push(todo);
-    this.setState({ todoList, todoItem: "" });
-  };
-
+class TodoApp extends Component {
   render() {
-    const { todoList, todoItem } = this.state;
     return (
       <div>
         <AppBar title="Todo App" />
         <div style={rightMargin}>
-          <AddTodo
-            todo={todoItem}
-            handleChange={this.handleChange}
-            add={this.addTodoToList}
-            onEnterKeyPress={this.onEnterKeyPress}
-          />
+          <AddTodo />
           <TodoList />
         </div>
       </div>
-    );
+    )
   }
 }
+
+TodoApp = connect()(TodoApp)
+
+export default TodoApp

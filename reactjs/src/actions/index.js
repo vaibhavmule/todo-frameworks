b7233f15ea132343dfc944c67f0667cee707c39d
Vaibhav Mule <vaibhavmule135@gmail.com>
2017-11-16 17:10:36 +0530

    add modify action

--- reactjs/src/actions/index.js
+++ reactjs/src/actions/index.js
@@ -1,12 +1,19 @@
 
 // constants
 export const ADD_TODO = 'ADD_TODO'
+export const MODIFY_TODO = 'MODIFY_TODO'
 
 // actions
-
 export const addTodo = todo => {
 	return {
 		type: ADD_TODO,
+		todo
+	}
+}
+
+export const modifyTodo = todo => {
+	return {
+		type: MODIFY_TODO,
 		todo
 	}
 }

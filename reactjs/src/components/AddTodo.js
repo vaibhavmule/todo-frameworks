07493ad27b13ed271aefd6583f4595b79540a4ea
Vaibhav Mule <vaibhavmule135@gmail.com>
2017-11-16 17:13:28 +0530

    modify AddTodo component as required

--- reactjs/src/components/AddTodo.js
+++ reactjs/src/components/AddTodo.js
@@ -1,29 +1,27 @@
-import React, { Component } from 'react';
+import React from 'react';
 import TextField from 'material-ui/TextField';
 import RaisedButton from 'material-ui/RaisedButton';
 
 import {rightMargin} from './Styles.js';
 
-
-export default class AddTodo extends Component {
-
-	render() {
-		const {todo, handleChange, add, onEnterKeyPress} = this.props
-		return (
-			<div>
-			  <TextField
-          floatingLabelText="Add Todo"
-          name='todoItem'
-          value={todo}
-          onChange={handleChange}
-	        onKeyPress={onEnterKeyPress}
-          />
-        <RaisedButton
-        	style={rightMargin}
-        	label="Add Todo"
-        	primary={true}
-        	onClick={() => add(todo)}/>
-      </div>
-		)
-	}
+const AddTodoComponent = ({ todo, add, onEnterKeyPress, onClick, onChange }) => {
+  return (
+    <div>
+      <TextField
+        floatingLabelText="Add Todo"
+        name='todoItem'
+        value={todo}
+        onChange={onChange}
+        onKeyPress={e => onEnterKeyPress(e, todo)}
+        />
+      <RaisedButton
+        style={rightMargin}
+        label="Add Todo"
+        primary={true}
+        onClick={() => onClick(todo)}
+        />
+    </div>
+  )
 }
+
+export default AddTodoComponent;

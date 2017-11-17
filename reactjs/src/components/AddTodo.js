f27820fbe9d50b1c7983e89d42da67bea5df0d7b
Vaibhav Mule <vaibhavmule135@gmail.com>
2017-11-17 11:52:42 +0530

    add custom classes and remove Styles

--- reactjs/src/components/AddTodo.js
+++ reactjs/src/components/AddTodo.js
@@ -2,24 +2,25 @@ import React from 'react';
 import TextField from 'material-ui/TextField';
 import RaisedButton from 'material-ui/RaisedButton';
 
-import {rightMargin} from './Styles.js';
-
 const AddTodoComponent = ({ todo, add, onEnterKeyPress, onClick, onChange }) => {
   return (
-    <div>
+    <div className="center">
+      <div className="width">
       <TextField
         floatingLabelText="Add Todo"
         name='todoItem'
         value={todo}
         onChange={onChange}
+        fullWidth={true}
         onKeyPress={e => !!todo && onEnterKeyPress(e, todo)}
         />
       <RaisedButton
-        style={rightMargin}
         label="Add Todo"
         primary={true}
+        className="addButton"
         onClick={() => !!todo && onClick(todo)}
         />
+      </div>
     </div>
   )
 }

36ef7325d822997fef6a50a5ad5d9f1900695448
Vaibhav Mule <vaibhavmule135@gmail.com>
2017-11-21 11:55:59 +0530

    add delete button

--- reactjs/src/components/TodoList.js
+++ reactjs/src/components/TodoList.js
@@ -1,11 +1,23 @@
 import React from 'react'
 import {List, ListItem} from 'material-ui/List'
+import IconButton from 'material-ui/IconButton';
+import Delete from 'material-ui/svg-icons/action/delete';
 
 const TodoListComponent = ({ todos }) => (
 	<div className="center">
 	  <List className="width">
 	    {todos.map(item => (
-				<ListItem key={item}>{item}</ListItem>
+				<ListItem 
+					key={item}
+					rightIconButton={
+						<IconButton
+        			tooltip="delete">
+        			<Delete />
+        		</IconButton>
+					}
+					>
+					{item}
+				</ListItem>
 	    ))}
 	  </List>
   </div>
